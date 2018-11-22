f045b2258f22762f5ad70a50edb785f134632a8d
DanielAronovich <aronovich_daniel@healthymize.com>
2018-11-22 09:55:57 +0200

    server js crushes

--- client/main.js
+++ client/main.js
@@ -10,42 +10,21 @@ form.addEventListener('submit', e => {
     let email = document.getElementById("EmailAddress").value;
     let password = document.getElementById("InputPassword").value;
 
-    let dataJSON = JSON.stringify( {name: email, age: password});
+    let dataJSON = JSON.stringify( {Email: email, Password: password});
 
 
-    // let timeString = form.action+"?format="+time_var;
-    console.log(dataJSON);
-
-    console.log(form.action);
 
     request('POST',form.action, data => {
+        document.querySelector("#response").innerHTML = data; }, {}, dataJSON );
 
-
-        document.querySelector("#response").innerHTML = data;
-
-
-
-    }, {}, dataJSON );
 });
 
-// function request(action, success,data) {
 
-//     let xhr = new XMLHttpRequest();
-//     xhr.open('POST', action);
-//     xhr.setRequestHeader()
-//     xhr.addEventListener('load', function (e) { success(e.currentTarget.responseText); });
-//     xhr.send(data);
-
-
-// }
 
 function request(method, url, success, reject, data) {
     let xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    // xhr.addEventListener('load', e => {
-    //     resolve(JSON.parse(e.target.responseText))
-    // });
     xhr.addEventListener('load', function (e) { success(e.currentTarget.responseText); });
 
     xhr.addEventListener('error', err => {
--- server/server.js
+++ server/server.js
@@ -16,13 +16,44 @@ let express = require('express');
 let app = express();
 const port = 8000
 let bodyParser = require('body-parser')
+let mongo = require('mongodb');
+let trainwise, users;
+
+mongo.MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if (err) {
+    console.error(`Error connecting to db [err=${err}]`);
+    return;
+  }
+
+  app.listen(port, () => console.log(`app listening on port ${port}!`))
+  trainwise = client.db('trainwise');
+  users = trainwise.collection('users');
+
+
+
+});
+
+
 
 app.use(express.static('../client'));
 
 app.use(bodyParser.json());
 
 app.post('/login',  (req, res,next) => {
-    res.send(`Got a POST request for login with age of: ${req.body.age} and name of ${req.body.name} `);
+
+    users.find({email: req.body.Email, password: req.body.password}).toArray((err, result) => {
+
+        if (err || result.length < 1) {
+            res.sendStatus(401);
+            return;   
+        }
+
+        res.redirect('/login.html')
+
+
+    });
+
+
     next();
 })
 
@@ -45,7 +76,6 @@ app.put('/videos/:videoID', function (req, res) {
 
 
 
-app.listen(port, () => console.log(`app listening on port ${port}!`))
 
 
 // app.listen(8000, () => console.log('listening on 8000...'));
